4c034ed9c3f62ac2ee14e6f287a61de6c9dbf8ee
nandeeshnan <nandeeshkv11@gmail.com>
2025-02-05 12:16:40 +0530

    removed duplicate route

--- networking.tf
+++ networking.tf
@@ -38,11 +38,7 @@ resource "aws_route_table_association" "public_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
-resource "aws_route" "public_route" {
-  route_table_id         = aws_route_table.public_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
+
 
 # Associate Public Subnet with Public Route Table
 resource "aws_route_table_association" "public_association" {
